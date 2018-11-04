e65cbe3db588eb935611d0cbf6e062f659f7ca2f
Sandro Lucas <osandrolucas@gmail.com>
2018-11-04 18:59:57 -0200

    Programa Ok, versão 1. Controle de alertas usando o tempo.

--- alert_WhatsApp.py
+++ alert_WhatsApp.py
@@ -1,15 +1,16 @@
-import time
 from datetime import datetime
+from time import sleep, strftime
+
 from twilio.rest import Client
 
-# Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'AC01b4a24a759e05f75c0a100c03663d7f'
-auth_token = 'a4ddc80414cd93d0e241e1baf19ea609'
+# Your Account Sid and Auth Token
+account_sid = '######'
+auth_token = '#######'
 client = Client(account_sid, auth_token)
 
 def envia_notificacao(a, s):
     message = client.messages.create(
-        body='*ALERTA:* bla bla bla.... Agora é {}, programa será encerrado em {} segundos!'.format(a, s),
+        body='*ALERTA:* bla bla bla.... Agora é {}, o programa será encerrado em {} segundos!'.format(a, s),
         from_='whatsapp:+14155238886',
         to='whatsapp:+555184373078'
     )
@@ -18,9 +19,7 @@ def envia_notificacao(a, s):
 def alerta_15(second_stop):
     print('ATENÇÃO: Faltam {} segundos encerrar!'.format(int(second_stop) - int(datetime.now().second)))
 
-print('Page of tests')
-
-print('Start Project: {}h\n'.format(time.strftime("%H:%M:%S")))
+print('Start Project: {}h\n'.format(strftime("%H:%M:%S")))
 
 hour_stop = input('Hour you want to stop the program (Ex.: 15): ')
 minute_stop = input('Minute you want to stop the program (Ex.: 47): ')
@@ -29,16 +28,16 @@ stop_program = hour_stop + ':' + minute_stop + ':' + second_stop
 
 print('\nStop Program at: {}h'.format(stop_program))
 
-print('\nExact time for now: {}h'.format(time.strftime("%H:%M:%S")))
+print('\nExact time for now: {}h'.format(strftime("%H:%M:%S")))
 
 print('\nStart of monitoring time...')
 
 while True:
-    print(time.strftime("%H:%M:%S"))
-    time.sleep(1)
+    print(strftime("%H:%M:%S"))
+    sleep(1)
     if (int(hour_stop) - int(datetime.now().hour)) == 0 and (int(minute_stop) - int(datetime.now().minute)) == 0 and (int(second_stop) - int(datetime.now().second)) == int(15):
         alerta_15(second_stop)
-        envia_notificacao(time.strftime("%H:%M:%S"), int(second_stop) - int(datetime.now().second))
-    if time.strftime("%H:%M:%S") == stop_program:
+        envia_notificacao(strftime("%H:%M:%S"), int(second_stop) - int(datetime.now().second))
+    if strftime("%H:%M:%S") == stop_program:
         print('END...\nClosing Program... Now is {}h.'.format(stop_program))
         break
